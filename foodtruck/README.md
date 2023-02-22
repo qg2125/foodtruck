e43f52b4e1e3671ef2b1314fb66e24172c9d51bc
geqing <geqing2012@hotmail.com>
2023-02-22 16:26:54 -0500

    add readme file and add 6 events

---
+++ foodtruck/README.md
@@ -0,0 +1,53 @@
+# Web Development Project 1 - *Food Truck*
+
+Submitted by: **Qing Ge**
+
+This web app: **This app presents popular food trucks in NYC**
+
+Time spent: **8** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [X] **There is a unique theme for events or resources relevant to a specific community**
+- [X] **At least 10 unique resources or events are displayed in a responsive card format**
+
+The following **optional** features are implemented:
+
+- [X] Buttons or links to a related resources are on each card component
+- [X] The site is responsive for both desktop and mobile formats
+
+The following **additional** features are implemented:
+
+* [ ] List anything else that you added to improve the site's functionality!
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented required features:
+
+<img src='https://i.imgur.com/S7M0XJi.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with ZOOM and Imgur  
+
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- foodtruck/src/App.jsx
+++ foodtruck/src/App.jsx
@@ -11,10 +11,16 @@ const App = () => {
       </div>
       
       <div className='container'>
-        <Card url = 'https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg' name = 'Birria-Landia' country = 'Mexican' menu = 'https://thebirrialandia.com/menu/' />
+        <Card url = 'https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg' name = 'Birria-Landia' country = 'Mexican' menu = 'https://birrialandia.com/menu' />
         <Card url = 'https://res.cloudinary.com/the-infatuation/image/upload/c_fill,w_1200,ar_4:3,g_auto,f_auto/cms/reviews/mysttik-masala/banners/1561989798.42' name = 'Mysttik Masala' country = 'Indian' menu = 'https://indianfoodny.com/'/>
         <Card url = 'https://nypost.com/wp-content/uploads/sites/2/2014/08/081114featureshalaguysbz-1.jpg?quality=75&strip=all&w=1024' name='The Halal Guys' country ='Middle Eastern' menu='https://thehalalguys.com/menu/' />
         <Card url = 'https://nydosas.com/wp-content/uploads/2018/07/maxresdefault-2-1024x630.jpg' name='NY Dosas' country ='Vegetarian' menu='https://nydosas.com/menu/' />
+        <Card url = 'https://infatuation.imgix.net/media/images/reviews/jerk-pan/banners/1561989308.8.jpg' name='Jerk Pan' country ='Jamaican' menu='https://www.allmenus.com/ny/new-york/360888-jerk-pan/menu/' />
+        <Card url = 'https://static01.nyt.com/images/2019/09/04/dining/29hungry-tong2/merlin_159654336_783be212-7726-485e-9250-4793a743af41-jumbo.jpg' name='Tong' country ='Bangladeshi' menu='https://www.facebook.com/Bangladeshistreetfoods/' />
+        <Card url = 'https://d3hbe0kmbam4a5.cloudfront.net/photos/67e1cdb7-bc27-4f26-8343-851bfaa7fc22.jpg' name="Uncle Gussy's" country ='Greek' menu='https://unclegussys.com/menus/' />
+        <Card url = 'https://infatuation.imgix.net/media/images/reviews/moms-momo/banners/1606245507.888788.png' name="Mom's Mono" country ='Tibetan' menu='https://www.restaurantji.com/ny/jackson-heights/moms-momo-/menu/' />
+        <Card url = 'https://fastly.4sqi.net/img/general/600x600/784042_29bZZHPGisHnWqyIuuXMo8D2zzW7QS6Hft7Vv15SH_I.jpg' name='Makina Cafe' country ='Ethiopian' menu='https://makinacafenyc.com/' />
+        <Card url = 'https://live.staticflickr.com/8088/8400188739_53b16c7d13_z.jpg' name='Patacon Pisao' country ='Venezuelan' menu='https://www.pataconpisaonyc.com/menus/' />
     </div>
     </div>
   )
